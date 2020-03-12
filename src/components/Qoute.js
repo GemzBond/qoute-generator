ba70c1b25d4463d5857d9e9707ccebb609235dc1
Regem Semilla <regem.semilla123@gmail.com>
2020-03-12 18:50:10 +0800

    Iniatializing some functionality

--- src/components/Qoute.js
+++ src/components/Qoute.js
@@ -1,8 +1,25 @@
 import React from "react"
 
+const API = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
 class Qoute extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      qoutes: [],
+      author:[]
+    }
+
+    // this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data.author)
+      })
   }
 
   render() {
@@ -16,7 +33,7 @@ class Qoute extends React.Component {
             <a className="border border-primary mt-3 p-2 rounded" id="tweet-qoute" href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
           </div>
           <div> 
-            <button className="border rounded border-info mt-3 p-2" id="new-qoute">Generate</button>
+            <button className="border rounded border-info mt-3 p-2" id="new-qoute" onClick={this.handleClick}>Generate</button>
           </div>
         </div>
       
