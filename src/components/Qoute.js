c865dbe3ab1fe4e6e68690c514e48f0895eb594e
Regem Semilla <regem.semilla123@gmail.com>
2020-03-12 18:17:13 +0800

    Added styling

--- src/components/Qoute.js
+++ src/components/Qoute.js
@@ -7,11 +7,19 @@ class Qoute extends React.Component {
 
   render() {
     return (
-      <div id="qoute-box" className="border">
-        <h1 id="text">"Sample Qoute"</h1>
-        <p id="author">-John Doe</p>
-        <a id="tweet-qoute" href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-        <button id="new-qoute">Generate</button>
+      <div id="qoute-box" className="bg-light border rounded mx-auto my-5 px-3 py-3 w-50">
+        <h1 className="text-center" id="text">"Easily make an element as wide or as tall (relative to its parent) with our width and height utilities."</h1>
+        <h4 className="text-right" id="author">-John Doe</h4>
+
+        <div className="d-flex flex-row justify-content-between">
+          <div className="mt-4">
+            <a className="border border-primary mt-3 p-2 rounded" id="tweet-qoute" href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+          </div>
+          <div> 
+            <button className="border rounded border-info mt-3 p-2" id="new-qoute">Generate</button>
+          </div>
+        </div>
+      
       </div>
     )
   }
